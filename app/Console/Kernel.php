674caf760779ff4c979bc85821564f8f9616dc61
Axisco91 <franciscohoskins@gmail.com>
2023-06-20 23:53:45 +0200

    Añadir hora al comando

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
 
-        $schedule->command('updateCoursesStatus')->daily();
+        $schedule->command('updateCoursesStatus')->dailyAt('00:00:00');
     }
 
     /**
