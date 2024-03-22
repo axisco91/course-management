629093c6abfc5406633a1285265b0b4b3981d804
AntonioJimenez <info@aztechnology.es>
2024-03-22 13:50:06 +0100

    falta por poblar y modificar aspecto V&RcontratoFormacionAlternancia y V&R acuerdoColaboracion

---
+++ resources/views/documents/V&RcontratoFormacion.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contrato Formación - V&R</title>
+    <style type="text/css">
+
+        .bg-green-100{
+            background-color: rgb(184, 206, 198) !important; 
+        }
+
+        .text-green-100{
+            color: rgb(184, 206, 198) !important; 
+        }
+
+        .bg-green-900{
+            background-color: rgb(24, 57, 46) !important; 
+        }
+
+        .text-green-900{
+            color: rgb(24, 57, 46) !important; 
+        }
+    </style>
+</head>
+<body class="p-5">
+
+    <div class="grid grid-cols-2 grid-flow-col gap-2">
+        <div class="col-span-11 flex justify-end">
+            <p class="p-1 m-2 rounded-full bg-green-900 text-white flex items-center">CONTRATO DE FORMACIÓN EN ALTERNANCIA</p>
+        </div>
+        <div class="col-span">
+            <img class="w-44" src="/V&R/logo.png" alt="">
+        </div>
+    </div>
+
+    <div>
+        <h1 class="text-xl text-green-800 m-2">PLANIFICACIÓN DE LA ACTIVIDAD FORMATIVA</h1>
+        <hr>
+        <section class="w-11/12 mx-auto mt-3">
+            <div class="flex flex-row justify-center">
+                <p class="text-green-900 font-bold text-xs ml-2">OCUPACIÓN: <span class="text-xs text-green-900 font-normal">(texto sustitutivo)</span></p>
+                <p class="text-green-900 font-bold text-xs ml-2">ALUMNO/A: <span class="text-xs text-green-900 font-normal">(texto sustitutivo)</span></p>
+                <p class="text-green-900 font-bold text-xs ml-2">DNI: <span class="text-xs text-green-900 font-normal">(texto sustitutivo)</span></p>
+            </div>
+            <div class="flex flex-row justify-center">
+                <p class="text-green-900 font-bold text-xs ml-2">Fecha inicio del contrato: <span class="text-xs text-green-900 font-normal">(texto sustitutivo)</span></p>
+                <p class="text-green-900 font-bold text-xs ml-2">Fecha de fin de contrato: <span class="text-xs text-green-900 font-normal">(texto sustitutivo)</span></p>
+                <p class="text-green-900 font-bold text-xs ml-2">Fecha de inicio actividad formativa: <span class="text-xs text-green-900 font-normal">(texto sustitutivo)</span></p>
+                <p class="text-green-900 font-bold text-xs ml-2">Fecha de fin actividad formativa: <span class="text-xs text-green-900 font-normal">(texto sustitutivo)</span></p>
+            </div>
+
+            <table class="mt-5 w-full border-solid border-2 border-green-900">
+                <thead class="text-left">
+                    <tr>
+                        <th colspan="2" class="px-6 py-3 text-xs text-green-900 font-bold">
+                            ESPECIALIDAD FORMATIVA <span class="font-normal">(horas)</span>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody class="text-center">
+                    <tr class="bg-green-900">
+                        <td colspan="2" class="px-6 py-3 text-xs text-green-100 font-bold">
+                            ESPECIALIDADES FORMATIVAS
+                        </td>
+                        <td colspan="5" class="px-6 py-3 text-xs text-green-100 font-bold">
+                            LUGAR Y FECHAS DE REALIZACIÓN DE LA ACCIÓN FORMATIVA
+                        </td>
+                    </tr>
+                    <tr class="bg-green-100 text-green-900 font-bold">
+                        <td>Código</td>
+                        <td class="border-r-2 border-l-2 border-green-900">Denominación</td>
+                        <td class="border-r-2 border-green-900">Centro de formación</td>
+                        <td class="border-r-2 border-green-900">Fecha Inicio / Fin</td>
+                        <td class="border-r-2 border-green-900">Horas Semana</td>
+                        <td class="border-r-2 border-green-900">Días Semana</td>
+                        <td>Horario</td>
+                    </tr>
+                </tbody>
+            </table>
+            <p class="text-xs" >Durante las vacaciones legalmente establecidas no se organizará ninguna actividad formativa. Días con derecho a su disfrute para el periodo contractual vigente: 6</p>
+        </section>
+        <div class="flex justify-end p-5 m-5">
+            <img src="/V&R/firma.PNG" alt="">
+        </div>
+        <div class="flex justify-center">
+            <p class="underline text-green-100 font-bold ml-2">EMPRESA: <span class="text-green-900 underline">(nombre)</span></p>
+            <p class="underline text-green-100 font-bold ml-2">TRABAJADOR/A: <span class="text-green-900 underline">(nombre)</span></p>
+            <p class="underline text-green-100 font-bold ml-2">CENTRO DE FORMACIÓN: <span class="text-green-900 underline">(nombre)</span></p>
+        </div>
+    </div>
+    
+</body>
+</html>
