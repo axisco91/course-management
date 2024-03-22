52d60177f8a08f139aeceb1eab53847c2fb761fb
Alazne <info@aztechnology.es>
2024-03-22 13:57:21 +0100

    certificadoBonificaciones poblados y realizado el diseño correctamente

--- resources/views/documents/V&RcertificadoBonificaciones.blade.php
+++ resources/views/documents/V&RcertificadoBonificaciones.blade.php
@@ -5,30 +5,77 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Certificado de Bonificaciones - V&R</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style>
+        h1{
+            font-weight: bold;
+            font-size: 2.0rem;
+        }
+
+        .icono img{
+            position: fixed;
+            right: 5%;
+            width: 150px
+        }
+
+        .subrayado{
+            text-decoration: underline;
+        }
+
+        .table-bordered thead th {
+            border-bottom: 2px solid rgb(168, 208, 141); /* Color y grosor del borde inferior */
+            border-top: none; /* Eliminar el borde superior */
+            border-left: none; /* Eliminar el borde izquierdo */
+            border-right: none; /* Eliminar el borde derecho */
+        }
+
+        .table-bordered tbody tr {
+            border-bottom: 2px solid rgb(239, 246, 234); /* Color y grosor del borde inferior */
+            border-top: none; /* Eliminar el borde superior */
+            border-left: none; /* Eliminar el borde izquierdo */
+            border-right: none; /* Eliminar el borde derecho */
+            text-align: center; 
+        }
+
+        .table-bordered tbody tr .borde {
+            border-bottom: none;  /* Color y grosor del borde inferior */
+            border-top: none; /* Eliminar el borde superior */
+            border-left: none; /* Eliminar el borde izquierdo */
+            border-right: 2px solid rgb(208, 229, 193); /* Eliminar el borde derecho */
+        }
+
+        .table-custom tbody tr:nth-child(odd) {
+            background-color: rgb(226, 239, 217); /* Color de fondo para las filas impares */
+        }
+
+        .table-custom tbody tr:nth-child(even) {
+            background-color: #ffffff; /* Color de fondo para las filas pares */
+        }
+
+    </style>
 </head>
 <body class="p-5">
 
     <!-- LOGO -->
-    <div class="flex justify-end">
-        <img width="150" src="./V&R/logo.png" alt="">
+    <div class="icono mb-4">
+        <img src="./V&R/logo.png" alt="">
     </div>
 
     <section>
         <!-- PRIMERA PARTE -->
-        <h1 class="font-bold text-2xl pb-3">CERTIFICADO DE BONIFICACIONES</h1>
+        <h1 class="text-center mt-3 pb-3">CERTIFICADO DE BONIFICACIONES</h1>
 
-        <article class="mt-4 mb-3" >
+        <article class="mt-4 mb-3 text-center">
             MV & JAR CONSULTORES, S.L., CON CIF: B72132988, con domicilio de notificaciones en C/ Real Fernando, local 4, código postal 11540,  Sanlúcar de Barrameda (Cádiz), representada por Don. Manuel 
             Villegas Rosa, mayor de edad con DNI: 79252530G, Centro Acreditado en el Registro Estatal para la 
             impartición  de  formación  con  el  N.º  de  registro  8000000645,  correo  electrónico  a  efectos  de 
-            notificaciones: info@vrconsultores.es, <span class="font-bold">CERTIFICA</span>: 
+            notificaciones: info@vrconsultores.es, <strong>CERTIFICA</strong>: 
             
             <div class="mt-3">
                 La contratación de los servicios de formación teórica en la modalidad de TELEFORMACIÓN inherente a 
                 un contrato para la Formación y el Aprendizaje en Alternancia con el siguiente detalle: 
-                <p>Empresa: <span class="underline">{{$company->name}}</span>– CIF <span class="underline">{{$company->nif}}</span> </p>
-                <p>Trabajador/a: <span class="underline">{{$trainingContract->student->name}} {{$trainingContract->student->surname}}</span> </p>
-                <p>Contratación con inicio el <span class="underline">{{$trainingContract->beginning}}</span>, y finalización el <span class="underline">{{$trainingContract->end}}</span>. </p>
+                <p>Empresa: <span class="subrayado">{{$company->name}}</span>– CIF <span class="subrayado">{{$company->nif}}</span> </p>
+                <p>Trabajador/a: <span class="subrayado">{{$trainingContract->student->name}} {{$trainingContract->student->surname}}</span> </p>
+                <p>Contratación con inicio el <span class="subrayado">{{$trainingContract->beginning}}</span>, y finalización el <span class="underline">{{$trainingContract->end}}</span>. </p>
             </div>    
             
             <div class="mt-3">
@@ -38,9 +85,9 @@
         </article>
 
         <!-- TABLA -->
-        <table class="mx-auto">
+        <table class="mx-auto table table-bordered table-custom">
             <thead>
-                <tr class="font-bold border-4 border-green-600">
+                <tr class="text-center font-bold">
                     <th>NOMBRE TRABAJADOR/A</th>
                     <th>DNI</th>
                     <th>F.INICIO</th>
@@ -53,27 +100,28 @@
             @foreach($bonus as $e)
                 @php
                     $monthKey = \Carbon\Carbon::parse($e->start)->format('Y-m');
+                    $sumaHoras += $monthlyFormationHours->{$monthKey} ?? 0;
                 @endphp
-                <tr class="border-b-2 border-green-600 text-center">
-                    <td>{{$trainingContract->student->name}} {{$trainingContract->student->surname}}</td>
-                    <td class="border-l-2 border-r-2 border-green-600">{{$trainingContract->student->dni}}</td>
-                    <td class="border-l-2 border-r-2 border-green-600">{{$e->start}}</td>
-                    <td class="border-l-2 border-r-2 border-green-600">{{$e->end}}</td>
-                    <td class="border-l-2 border-r-2 border-green-600">
+                <tr>
+                    <td class="borde">{{$trainingContract->student->name}} {{$trainingContract->student->surname}}</td>
+                    <td class="borde">{{$trainingContract->student->dni}}</td>
+                    <td class="borde">{{$e->start}}</td>
+                    <td class="borde">{{$e->end}}</td>
+                    <td class="borde">
                         {{ $monthlyFormationHours->{$monthKey} ?? 'N/A' }}
                     </td>
-                    <td >(importe)</td>
+                    <td >{{ $monthlyFormationHours->{$monthKey}*5 ?? 'N/A' }}</td>
                 </tr>
             @endforeach
             </tbody>
         </table>
 
         <div class="text-end mt-3 mr-5 pr-5">
-            <p class="text-lg">Total, bonificaciones de la empresa: (suma horas)h (suma importe)€</p>
+            <p class="text-lg">Total, bonificaciones de la empresa: {{$sumaHoras}} h {{$sumaHoras*5}} €</p>
         </div>
 
         <!-- SEGUNDA PARTE -->
-        <h2 class="font-bold text-xl pb-3">IMPORTANTE:</h2>
+        <h2 class="font-bold pb-3">IMPORTANTE:</h2>
 
         <article class="mt-4 mb-3">
             Si durante la duración del contrato surge cualquier situación que suponga una modificación una baja 
@@ -82,7 +130,7 @@
             que nos informe de manera inmediata; para que podamos paralizar y/o reanudar correctamente la 
             formación. Al mismo tiempo, nos debe remitir los documentos justificativos de dicha situación para 
             proceder a comunicarla al Servicio Público de Empleo competente.
-            <p>Y para que sirva al interesado como justificante, se expide el presente documento en Sanlúcar de Barrameda, a 05 de febrero de 2024.</p>
+            <p>Y para que sirva al interesado como justificante, se expide el presente documento en Sanlúcar de Barrameda, a <span class="subrayado">05</span> de febrero de <span class="subrayado">2024 </span>.</p>
         </article>
 
         <!-- FIRMA -->
@@ -92,7 +140,7 @@
 
     <footer class="pt-5 mt-5 text-center">
         <p>MV & JAR CONSULTORES, S.L. – B72132988 – C/ REAL FERNANDO, Local 4 – 11.540 SANLÚCAR DE BDA. (CÁDIZ) </p>
-        <p>Contacto: Silvia Arcas - 651 926 502 – 956 367 562 – <a href="#" class="text-blue-500 underline hover:text-blue-700">silvia.arcas@vrconsultores.es</a> – vrconsultores.es</p>
+        <p>Contacto: Silvia Arcas - 651 926 502 – 956 367 562 – <a href="#">silvia.arcas@vrconsultores.es</a> – vrconsultores.es</p>
     </footer>
 </body>
 </html>
