7340ba6546aed2f5d459920291797481d56c1721
Alazne <info@aztechnology.es>
2024-04-02 16:19:10 +0200

    modelo de agreement

--- app/Models/ApplicableAgreement.php
+++ app/Models/ApplicableAgreement.php
@@ -4,8 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\trainingContracts;
+    
 class ApplicableAgreement extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'code',
+        'annually_hours',
+        'agreement_type_id',  // Futura clave foránea
+    ];
+
+    /**
+     * Get the agreement type associated with the applicable agreement.
+     */
+    public function agreementType()
+    {
+        return $this->belongsTo(AgreementType::class);
+    }
+    public function trainingContracts()
+    {
+        return $this->hasMany(TrainingContract::class);
+    }
 }
