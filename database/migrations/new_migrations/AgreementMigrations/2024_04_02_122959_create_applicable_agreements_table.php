b1430aa323b6332492526e0bb2310a27e5f1bca5
Alazne <info@aztechnology.es>
2024-04-02 16:16:17 +0200

    migraciones de aplicable agreements

--- database/migrations/new_migrations/AgreementMigrations/2024_04_02_122959_create_applicable_agreements_table.php
+++ database/migrations/new_migrations/AgreementMigrations/2024_04_02_122959_create_applicable_agreements_table.php
@@ -15,8 +15,14 @@ class CreateApplicableAgreementsTable extends Migration
     {
         Schema::create('applicable_agreements', function (Blueprint $table) {
             $table->id();
-            
+            $table->string('name');
+            $table->string('code');
+            $table->integer('annually_hours');
+            $table->unsignedBigInteger('agreement_type_id'); // Futura clave foránea
             $table->timestamps();
+
+             // Definición de la clave foránea (descomenta cuando la tabla agreement_types esté lista)
+            // $table->foreign('agreement_type_id')->references('id')->on('agreement_types');
         });
     }
 
