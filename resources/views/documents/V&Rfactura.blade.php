65c3afa83d5572a7248c19a1451982c1d2c34639
AntonioJimenez <info@aztechnology.es>
2024-05-20 10:11:51 +0200

    fecha variable

--- resources/views/documents/V&Rfactura.blade.php
+++ resources/views/documents/V&Rfactura.blade.php
@@ -60,14 +60,14 @@
         <table style="width:100%">
             <tr>
                 <td class="menos-margin-bottom-2" style="width:85%; border: none;">
-                    <p><b><span class="letra-verde">Modalidad: </span></b> Teleformación </p>
-                    <p><b><span class="letra-verde">Ocupación: </span></b> {{$occupation->name}} </p>
-                    <p><b><span class="letra-verde">Trabajador/ a: </span></b> {{$student->name}} {{$student->surname}} <b><span class="letra-verde">DNI: </span></b> {{$student->dni}} </p>
-                    <p><b><span class="letra-verde">Fecha inicio y fecha fin de la formación: </span></b> 
+                    <p><b><span class="letra-verde">Modalidad:</span></b> Teleformación </p>
+                    <p><b><span class="letra-verde">Ocupación:</span></b> {{$occupation->name}} </p>
+                    <p><b><span class="letra-verde">Trabajador/ a:</span></b> {{$student->name}} {{$student->surname}} <b><span class="letra-verde">DNI:</span></b> {{$student->dni}} </p>
+                    <p><b><span class="letra-verde">Fecha inicio y fecha fin de la formación:</span></b> 
                         {{ \DateTime::createFromFormat('Y-m-d', $trainingContractBonus->start)->format('d/m/Y') }} - 
                         {{ \DateTime::createFromFormat('Y-m-d', $trainingContractBonus->end)->format('d/m/Y') }}  
                     </p>
-                    <p><b><span class="letra-verde">Horas: </span></b> {{$trainingContractBill->hours}} </p>
+                    <p><b><span class="letra-verde">Horas:</span></b> {{$trainingContractBill->hours}} </p>
 
                 </td>
                 <td class="menos-margin-bottom-2" style="width:15%; border: none; vertical-align: top; text-align: center">
@@ -121,7 +121,7 @@
         </table>
         
 
-        <p class="mt-4"><b>Aplicar en los seguros sociales del mes de:</b> enero del 2024 (que se presentan en el mes siguiente)</p>
+        <p class="mt-4"><b>Aplicar en los seguros sociales del mes de:</b> {{ \Carbon\Carbon::now()->locale('es')->isoFormat('MMMM [de] Y') }} (que se presentan en el mes siguiente)</p>
         <p class="mt-2"> Los costes de la formación objeto de bonificación deberán quedar expresamente identificados en la 
             contabilidad de la empresa (Orden ESS/2518/2013 art. 9.3).</p>
         <p class="mt-2 letra-verde-pequena"> <b>MV & JAR Consultores, S.L. es el Responsable del tratamiento de los datos personales proporcionados bajo su consentimiento y le informa de que estos datos serán tratados de conformidad con lo dispuesto en el Reglamento (UE) 2016/679, de 27 de abril (GDPR), y la Ley Orgánica 3/2018, de 5 de diciembre (LOPDGDD), con la finalidad de mantener una relación de servicios  y conservarlos durante no más tiempo del necesario para mantener el fin del tratamiento o mientras existan prescripciones legales que dictaminen su custodia. No se comunicarán los datos a terceros, salvo obligación legal. Asimismo, se le informa de que puede ejercer los derechos de acceso, rectificación, portabilidad y supresión de sus datos y los de limitación y oposición a su tratamiento dirigiéndose a MV & JAR Consultores, S.L. en C/ Real Fernando, local 4, - 11540 Sanlúcar de Barrameda (Cádiz) Email: <a href="mailto:info@vrconsultores.es"></a> info@vrconsultores.es y el de reclamación a <a href="www.aepd.es."><u>www.aepd.es.</u></a></p>
