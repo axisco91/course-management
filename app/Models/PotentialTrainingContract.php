863086373cbd8de88c5c1023a728bd0ca9a9c82d
Angel Caparros <info@aztechnology.es>
2024-05-20 13:31:44 +0200

    potential training contract

--- app/Models/PotentialTrainingContract.php
+++ app/Models/PotentialTrainingContract.php
@@ -32,65 +32,13 @@ Class PotentialTrainingContract extends Model
         'training_schedule',
         'working_schedule',
         'full_schedule',
-        'converted',
-        'comment'
+        'monday',
+        'tuesday',
+        'wednesday',
+        'thursday',
+        'friday',
+        'saturday',
+        'sunday',
     ];
-
-    /**
-     * @return \Illuminate\Database\Eloquent\Relations\HasOne
-     */
-    public function levelStudy()
-    {
-        return $this->hasOne('App\Models\LevelStudy', 'id', 'level_study_id');
-    }
-
-    /**
-     * @return \Illuminate\Database\Eloquent\Relations\HasOne
-     */
-    public function population()
-    {
-        return $this->hasOne('App\Models\Population', 'id', 'population_id');
-    }
-
-    /**
-     * @return \Illuminate\Database\Eloquent\Relations\HasOne
-     */
-    public function professionalCategory()
-    {
-        return $this->hasOne('App\Models\ProfessionalCategory', 'id', 'professional_category_id');
-    }
-
-    /**
-     * @return \Illuminate\Database\Eloquent\Relations\HasOne
-     */
-    public function province()
-    {
-        return $this->hasOne('App\Models\Province', 'id', 'province_id');
-    }
-
-    /**
-     * @return \Illuminate\Database\Eloquent\Relations\HasOne
-     */
-    public function trainingAction()
-    {
-        return $this->hasOne('App\Models\TrainingAction', 'id', 'training_action_id');
-    }
-
-    /**
-     * @return \Illuminate\Database\Eloquent\Relations\HasOne
-     */
-    public function professionalFamily()
-    {
-        return $this->hasOne('App\Models\ProfessionalFamily', 'id', 'professional_family_id');
-    }
-
-    /**
-     * @return \Illuminate\Database\Eloquent\Relations\HasOne
-     */
-    public function professionalArea()
-    {
-        return $this->hasOne('App\Models\ProfessionalArea', 'id', 'professional_area_id');
-    }
-
 
 }
