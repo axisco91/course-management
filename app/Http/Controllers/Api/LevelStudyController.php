9a7ec89e5ac133c84a32901bd174a48fc32a7342
AzT3chnology <140794519+AzT3chnology@users.noreply.github.com>
2024-08-14 11:59:40 +0200

    feat(levelstudies): añadido code fillable
    Creación del fillable code para el nivel de estudios
        - Modificado Modelo
        - Modificado el Controlador
        - Creada una migración para añadir campo a  la bd.
        - Modificado el archivo de rutas para cambiar edit a put

--- app/Http/Controllers/Api/LevelStudyController.php
+++ app/Http/Controllers/Api/LevelStudyController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Api;
+
 use App\Models\LevelStudy;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -8,6 +9,13 @@ use Illuminate\Support\Facades\Validator;
 
 class LevelStudyController extends BaseController
 {
+    /**
+     * Obtiene todos los niveles de estudio.
+     *
+     * Este método devuelve una lista de todos los niveles de estudio existentes en el sistema.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function levelStudies() {
         try {
             return LevelStudy::getLevelStudies();
@@ -18,52 +26,113 @@ class LevelStudyController extends BaseController
         }
     }
 
+    /**
+     * Crea un nuevo nivel de estudio.
+     *
+     * Valida los datos de entrada y crea un nuevo registro en la tabla 'level_studies'.
+     * Retorna el nivel de estudio recién creado.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function create(Request $request){
         try {
-            $level_study = LevelStudy::createLevelStudy($request);
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|max:255',
+                'code' => 'required|string|size:2|unique:level_studies,code'
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => 400,
+                    'message' => $validator->errors()
+                ]);
+            }
+
+            $level_study = LevelStudy::createLevelStudy($request->all());
         } catch (\Exception $e){
             return response()->json([
                 'status' => 400,
                 'message' => $e->getMessage()
             ]);
         }
-
         return response()->json([
             'status' => 200,
             'level_study' => LevelStudy::getLevelStudy($level_study->id)
         ]);
     }
-
-    public function edit($id, Request $request){
+    
+    /**
+     * Obtiene un nivel de estudio por su ID.
+     * 
+     * Este método devuelve un nivel de estudio específico basado en el ID proporcionado.
+     * 
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Exception
+     * 
+     */
+    public function show($id)
+    {
         try {
-            $level_study = LevelStudy::updateLevelStudy($id, $request);
-        } catch (\Exception $e){
-            return response()->json([
-                'status' => 400,
-                'message' => $e->getMessage()
-            ]);
-        }
-
-        return response()->json([
-            'status' => 200,
-            'level_study' => LevelStudy::getLevelStudy($level_study->id)
-        ]);
-    }
-
-    public function getLevelStudy($id){
-        $level_study = LevelStudy::getLevelStudy($id);
-        if ($level_study) {
+            $levelStudy = LevelStudy::getLevelStudy($id);
             return response()->json([
                 'status' => 200,
-                'level_study' => $level_study
+                'level_study' => $levelStudy
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 400,
+                'message' => $e->getMessage()
+            ]);
+        }
+    }
+    /**
+     * Edita un nivel de estudio existente.
+     *
+     * Valida los datos de entrada y actualiza un nivel de estudio con el ID proporcionado.
+     * Retorna el nivel de estudio actualizado.
+     *
+     * @param int $id
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function edit($id, Request $request){
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|max:255',
+                'code' => 'required|string|size:2|unique:level_studies,code,'.$id
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => 400,
+                    'message' => $validator->errors()
+                ]);
+            }
+
+            $level_study = LevelStudy::updateLevelStudy($id, $request->all());
+        } catch (\Exception $e){
+            return response()->json([
+                'status' => 400,
+                'message' => $e->getMessage()
             ]);
         }
         return response()->json([
-            'status' => 400,
-            'message' => 'Nivel no existe'
+            'status' => 200,
+            'level_study' => LevelStudy::getLevelStudy($level_study->id)
         ]);
     }
 
+    /**
+     * Elimina un nivel de estudio.
+     *
+     * Elimina un registro de la tabla 'level_studies' basado en el ID proporcionado.
+     * Retorna una respuesta indicando el éxito o fracaso de la operación.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function destroy($id){
         if ($id) {
             try {
@@ -80,6 +149,13 @@ class LevelStudyController extends BaseController
         }
     }
 
+    /**
+     * Cuenta el número total de niveles de estudio.
+     *
+     * Devuelve el conteo total de registros en la tabla 'level_studies'.
+     *
+     * @return int
+     */
     public function count(){
         return LevelStudy::count();
     }
--- app/Models/LevelStudy.php
+++ app/Models/LevelStudy.php
@@ -13,9 +13,13 @@ class LevelStudy extends Model
 
     protected $table = 'level_studies';
 
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'code'];
 
     /**
+     * Relación uno a muchos con el modelo Student.
+     *
+     * Un nivel de estudio puede estar asociado con múltiples estudiantes.
+     *
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
     public function students()
@@ -23,45 +27,72 @@ class LevelStudy extends Model
         return $this->hasMany('App\Models\Student', 'level_study_id', 'id');
     }
 
+    /**
+     * Obtiene todos los niveles de estudio.
+     *
+     * Recupera todos los registros de niveles de estudio, agregando un campo 'used' que indica 
+     * si el nivel de estudio está asociado a algún estudiante.
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
     public static function getLevelStudies(){
         $level_studies = LevelStudy::select('*', 'id as value', 'name as label')
             ->get();
         foreach ($level_studies as $level_study){
             $student = Student::where('level_study_id', $level_study['id'])->first();
-            if ($student){
-                $level_study['used'] = true;
-            } else {
-                $level_study['used'] = false;
-            }
+            $level_study['used'] = $student ? true : false;
         }
         return $level_studies;
     }
 
+    /**
+     * Obtiene un nivel de estudio por su ID.
+     *
+     * Recupera un registro de nivel de estudio por su ID, agregando un campo 'used' que indica 
+     * si el nivel de estudio está asociado a algún estudiante.
+     *
+     * @param int $id
+     * @return \App\Models\LevelStudy|null
+     */
     public static function getLevelStudy($id){
         $level_study = LevelStudy::select('*', 'id as value', 'name as label')
             ->where('id', $id)->first();
         $student = Student::where('level_study_id', $level_study['id'])->first();
-        if ($student){
-            $level_study['used'] = true;
-        } else {
-            $level_study['used'] = false;
-        }
+        $level_study['used'] = $student ? true : false;
         return $level_study;
     }
 
-
+    /**
+     * Crea un nuevo nivel de estudio.
+     *
+     * Crea un nuevo registro en la tabla 'level_studies' con los datos proporcionados.
+     *
+     * @param array $data
+     * @return \App\Models\LevelStudy
+     */
     public static function createLevelStudy($data){
         $level_study = LevelStudy::create([
-            'name' => $data['name']
+            'name' => $data['name'],
+            'code' => $data['code']
         ]);
 
         return $level_study;
     }
 
+    /**
+     * Actualiza un nivel de estudio existente.
+     *
+     * Actualiza un registro en la tabla 'level_studies' con los datos proporcionados.
+     *
+     * @param int $id
+     * @param array $data
+     * @return \App\Models\LevelStudy
+     */
     public static function updateLevelStudy($id, $data){
         $level_study = LevelStudy::find($id);
         $level_study->update([
-            'name' => $data['name']
+            'name' => $data['name'],
+            'code'=> $data['code']
         ]);
 
         return $level_study;
